8c7c62c9d24b7b50ae78ec1b02c2699e39ef5ecd
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-29 08:59:32 -0300

    Componentizando.Importando o cabeçalho.

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -1,7 +1,9 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 export default function App(){
   return(
     <div>
-      <h1>Componente App</h1>
+      <Cabecalho/>
     </div>
   );
 }
