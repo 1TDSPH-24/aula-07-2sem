edb695af6dd4dedd34c21fda7157e080b3813ef8
ovitortadeu <souavitor6@gmail.com>
2024-08-29 09:52:00 -0300

    Deus é bom demais

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -3,10 +3,39 @@ import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
 
 export default function App(){
+
+    //Mudando o título da página
+    document.title = "Página principal";
+
+    //Exemplo de como tipar uma informação!!!
+    const tituloDapagina:string = "Minha Página";
+
+    const arrayNomes:string[] = ["jorge","morte","benio","vitor"]
+
+    const umObj:{nome:string;idade:number;br:boolean;casado:boolean} = {
+      nome: "João",
+      idade: 30,
+      br:true,
+      casado:false
+    }
+
+    // aplicando decruturing 
+    const {nome,idade,br,casado} = umObj;
+
+
   return(
     <div>
-      <Cabecalho/>
-      <Conteudo/>
+        <div>
+          <p>Nome : {umObj.nome}</p>
+          <p>Idade : {umObj.idade}</p>
+          <p>DESTRUCTURING</p>
+          <p>nome: {nome}</p>
+          <p>idade: {idade}</p>
+          <p>brasileiro: {br}</p>
+          <p>casado: {casado}</p>
+        </div>
+      <Cabecalho tituloProps={tituloDapagina}/>
+        <Conteudo/>
       <Rodape/>
     </div>
   );
