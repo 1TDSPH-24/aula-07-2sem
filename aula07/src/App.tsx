b81b5fa0ab2fa251b0d11de3b23adbf11d6eddba
enricodelguerra <ricodelguerra@gmail.com>
2024-08-29 08:59:50 -0300

    Boiler plate criado

---
+++ aula07/src/App.tsx
@@ -0,0 +1,9 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+export default function App(){
+  return(
+    <div>
+      <Cabecalho/>
+    </div>
+  );
+}
---
+++ aula07/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,7 @@
+export default function Cabecalho(){
+    return(
+        <div>
+            <header>Cabeçalho</header>
+        </div>
+    );
+}
