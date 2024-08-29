6d64b0c8768eaf87ac998c8b35db57fabbda65e3
enricodelguerra <ricodelguerra@gmail.com>
2024-08-29 09:06:00 -0300

    Conteudo criado

---
+++ aula07/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,9 @@
+export default function Conteudo() {
+    return(
+        <main>
+            <h2>Aqui fica todo o conteudo</h2>
+            <p>este é um paragrafo de teste</p>
+            <p></p>
+        </main>
+    );
+}
