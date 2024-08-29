dffe69e96fb3bf666b2e198f79abdcb0ded4158c
marianaroberti <mariananeri06@gmail.com>
2024-08-29 09:52:55 -0300

    Exemplo Props

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -3,9 +3,36 @@ import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
 
 export default function App() {
+
+  //Mudando o título da página
+  document.title = "Página principal";
+
+  //Exemplo de como tipar uma informação!!!
+  const tituloDapagina: string = "Minha Página";
+
+  const arrayNomes: string[] = ["Jorge", "Antonio", "Roberto"];
+
+  const umObj: { nome: string; idade: number; br: boolean; casado: boolean } = {
+    nome: "João",
+    idade: 30,
+    br: true,
+    casado: false
+  }
+
+  //Aplicando o destructuring
+  const { nome, idade, br, casado } = umObj;
+  console.log(br);
+
   return (
     <div>
-      <Cabecalho />
+      <div>
+        <p>Nome : {umObj.nome}</p>
+        <p>Idade : {umObj.idade}</p>
+        <p>DESTRUCTURING</p>
+        <p>Nome : {nome}</p>
+        <p>Idade : {idade}</p>
+      </div>
+      <Cabecalho tituloProps={tituloDapagina} arrayProps={arrayNomes} />
       <Conteudo />
       <Rodape />
     </div>
--- aula07/src/components/Cabecalho/Cabecalho.tsx
+++ aula07/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,13 @@
-export default function Cabecalho() {
+export default function Cabecalho({ tituloProps, arrayProps }: { tituloProps: string, arrayProps: string[] }) {
     return (
         <header>
             <h1>Cabeçalho</h1>
+            <h2>{tituloProps}</h2>
+            <div>
+                {arrayProps.map((item, index) => {
+                    return <p key={index}>{item}</p>
+                })}
+            </div>
         </header>
     );
 }
