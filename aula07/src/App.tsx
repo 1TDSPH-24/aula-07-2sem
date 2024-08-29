ff90d3f98e2750c07c67a3576cae7902691e2674
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-29 10:09:30 -0300

    Atualizando com array de Objetos

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -2,6 +2,13 @@ import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Conteudo from "./components/Conteudo/Conteudo";
 import Rodape from "./components/Rodape/Rodape";
 
+//Criando um tipo para o array de Objetos:
+type Pessoa = {
+  id: number;
+  nome: string;
+  idade: number;
+  }
+
 export default function App(){
 
     //Mudando o título da página
@@ -11,6 +18,13 @@ export default function App(){
     const tituloDapagina:string = "Minha Página";
 
     const arrayNomes:string[] = ["Jorge","Antonio","Roberto"];
+
+    //Criando um array de objetos para enviar como props para o conteudo
+    const arrayDeObjetos:Pessoa[] = [
+      {id:1,nome:"Jorge",idade:30},
+      {id:2,nome:"Antonio",idade:25},
+      {id:3,nome:"Roberto",idade:35}
+      ];
 
     const umObj:{nome:string;idade:number;br:boolean;casado:boolean} = {
       nome: "João",
@@ -33,7 +47,7 @@ export default function App(){
           <p>Idade : {idade}</p>
         </div>
       <Cabecalho tituloProps={tituloDapagina} arrayProps={arrayNomes}/>
-        <Conteudo/>
+        <Conteudo arrayObjProps={arrayDeObjetos}/>
       <Rodape/>
     </div>
   );
--- aula07/src/components/Conteudo/Conteudo.tsx
+++ aula07/src/components/Conteudo/Conteudo.tsx
@@ -1,9 +1,25 @@
-export default function Conteudo(){
+type Pessoa = {
+    id: number;
+    nome: string;
+    idade: number;
+}
+
+export default function Conteudo({arrayObjProps}:{arrayObjProps:Pessoa[]}){
     return (
         <main>
             <h2>Aqui fica todo o conteúdo!!</h2>
             <p>Este é um parágrafo de teste</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam modi reiciendis nesciunt inventore libero explicabo tempora veritatis deserunt deleniti facilis atque, ut rerum a accusantium illum esse dolores aspernatur porro delectus non? Ducimus, magni iste provident iure iusto possimus hic qui officia quam ex doloribus culpa non cumque praesentium vel.</p>
+
+                <div>
+                    { arrayObjProps.map((obj)=>(
+                        <div key={obj.id}>
+                            <span>{obj.nome}</span> -  
+                            <span>{obj.idade}</span>
+                        </div>
+                    )) }
+                </div>
+
         </main>
     );
 }
