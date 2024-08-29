d459a3444f99c4745f456533289774b403a7daa9
Rafa130206 <rafabompadre@gmail.com>
2024-08-29 09:04:08 -0300

    Componentes rodape e conteudo criados.

---
+++ aula07/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,9 @@
+export default function Conteudo(){
+    return(
+        <main>
+            <h2>Aqui fica todo o conteúdo</h2>
+            <p>Este é um parágrafo de teste</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam ipsum consectetur officia nostrum animi aperiam iusto eveniet illum accusamus odio. Exercitationem repudiandae minus qui aliquam, cupiditate reiciendis debitis nihil tempore in facere hic veritatis praesentium consequuntur optio eius neque autem ipsum labore doloremque. Optio dolores pariatur voluptatem obcaecati ab porro.</p>
+        </main>
+    )
+}
---
+++ aula07/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,7 @@
+export default function Rodape(){
+    return(
+        <div>
+            <footer>FIAP 2024<br /> Rafael Bompadre Lima</footer>
+        </div>
+    )
+}
