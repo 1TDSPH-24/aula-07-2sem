75ed32c55b2cd01547abc0c8ab28a15cc837037f
gusandrini <gustavosandrini92@gmail.com>
2024-08-29 09:50:30 -0300

    consts

--- aula07/src/App.tsx
+++ aula07/src/App.tsx
@@ -4,9 +4,33 @@ import Rodape from "./components/Rodape/Rodape";
 
 
 export default function App(){
+
+    //mudando otitulo da página
+    document.title = "Página principal";
+
+    //exemplo de como tipar uma informação!!!
+    const tituloDaPagina:string = "Minha página";
+
+    const arrayNomes:string[] = ["Jorge", "Antonio", "Roberto"]
+
+    const umObj:{nome:string;idade:number;br:boolean;casado:boolean} = {
+      nome: "João", 
+      idade: 30,
+      br:true,
+      casado:false
+    }
+
+    //aplicando o destructuring
+    const{nome, idade, br, casado} = umObj;
+
   return(
     <div>
-      <Cabecalho/>
+      <p>Nome: {umObj.nome}</p>
+      <p>Idade: {umObj.idade}</p>
+      <p>DESTRUCTURING</p>
+      <p>Nome : {nome}</p>
+      <p>Idade : {idade}</p>
+      <Cabecalho tituloProps={tituloDaPagina} arrayProps={arrayNomes}/>
         <Conteudo/>
       <Rodape/>
     </div>
